18e55d5c7ee4e83e3f150648ee8c59ce51c2adc5
naveenneerudi1475@gmail.com <naveenneerudi1475@gmail.com>
2026-03-08 20:46:31 +0530

    code is Modified

--- Task1.py
+++ Task1.py
@@ -1,13 +1,46 @@
 
 def factorial(num):
-    if num == 1: # base case - factorial of 1 is 1, stops recursion
+    """
+    calculates the factorial of a given number recursively.
+
+    parameter:
+    num(int): a non-negative integer whose factorial is to be calculated 
+
+    Returns:
+    int: factorial of the given number
+
+    Examples:
+    factorial(5) -> 120
+    factorial(0) -> 1
+    """
+
+    if num == 0 or num == 1: # base case - factorial of 1 is 1, stops recursion
         return 1
+    
+    elif num < 0: # handling negative numbers
+        return None
+    
     else:
         fact = num * factorial(num - 1) # recursive call - multiplying num with factorial of previous number
         return fact # returning the calculated factorial value
     
-num = int(input("enter a number: ")) # taking user input of a number
+def main():
+    """
+    Main function to run the factorial program.
 
-fact = factorial(num) # storing the return value of factorial function
+    """
+    try:
 
-print(f"Factorial of {num} is: {fact}") # printing the final result
+        num = int(input("enter a number: ")) # taking user input of a number
+
+        if num < 0: # input validation for negative numbers 
+            print("Error: Factorial is not defined for negative numbers. ")
+        else:
+            fact = factorial (num) # storing return value 
+            print(f"Factorial of {num} is: {fact}") # printing final result
+        
+    except ValueError: # handles non -integer inputs 
+        print("Error: please enter a valid whole number.")
+
+if __name__ == "__main__":
+    main()
--- Task2.py
+++ Task2.py
@@ -1,15 +1,54 @@
 import math
 
-num = int(input("enter a number: ")) # taking user input of a number
+def calculate_math(num):
+    """
+    calculates square root, logarithm and sine of a given number.
 
-square_root = math.sqrt(num) # evaluatig square root of num
+    parameter:
+        num(int/float): a positive number for calculations
+    Returns:
+        None: prints the calculated values
+    
+    Examples:
+        calculate_math(9) -> suare root: 3.0, Logarithm: 2.197, Sine: 0.412
 
-logarithm = math.log(num) # evaluating logarithm of num
+    """
+    square_root = math.sqrt(num) # evaluatig square root of num
 
-sine_of_num = math.sin(num) # evaluating sine of num
+    logarithm = math.log(num) # evaluating logarithm of num
 
-print(f"Square root: {square_root}")
+    sine_of_num = math.sin(num) # evaluating sine of num
 
-print(f"Logarithm: {logarithm}")
+    print(f"Square root: {square_root}")
 
-print(f"Sine: {sine_of_num}")
+    print(f"Logarithm: {logarithm}")
+
+    print(f"Sine: {sine_of_num}")
+
+
+
+def main():
+    """
+    Main function to run the math calculations program.
+    """
+    try:
+        num = int(input("enter a number: ")) # taking user input of a number
+
+        if num <= 0: # input validation -log and sqrt not defined for negative numbers and zero
+
+            print("Error: Please enter a positive number greater than 0.")
+        else:
+            calculate_math(num) # calling calculate_math function
+    
+    except ValueError:
+        print("Error: Please enter a valid whole number.")
+
+if __name__=="__main__": # runs only when files executed directly
+    main()
+
+
+
+
+
+
+
